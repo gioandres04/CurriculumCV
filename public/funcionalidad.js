fb274c5c47cc4b3cf8d2545a6c7649177db3302c
gioandres04 <167589145+gioandres04@users.noreply.github.com>
2024-06-22 13:07:35 -0400

    Add files via upload

---
+++ public/funcionalidad.js
@@ -0,0 +1,23 @@
+
+
+const formularioo = document.querySelector('.form');
+
+formularioo.addEventListener('submit',()=>{
+  Swal.fire(
+    {
+      title: "Datos Enviados exitosamente ",
+      text: "Te estaremos contactando pronto, gracias!",
+      icon: "success",
+      className: "my-swal-success"
+    }
+  )
+  .then(res=>{
+    fetch('/redireccionar')
+    .then(res=> res.json())
+    .then(res=>{
+      if(res.message == true){
+        window.location.href = '/';
+      }
+    })
+  })  
+});
